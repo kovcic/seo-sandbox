f03d208a9563e7ec59a98409df7d93bf209ccbe7
Andrej <kovcic@gmail.com>
2017-02-21 13:08:23 +0100

    using xdr

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ajax from 'xdr';
 
 const delay = {
     choMessage: 10,
@@ -17,7 +18,9 @@ class App extends Component {
             choMessage: null,
             faq1: null,
             faq2: null,
-            faq3: null
+            faq3: null,
+            user: null,
+            err: null
         };
         window.setTimeout(() => this.setState(Object.assign(this.state, {
             choMessage: 'Trees cover up a multitude of sins. All kinds of happy little splashes. And just raise cain. When things happen - enjoy them. They\'re little gifts. It\'s important to me that you\'re happy. Let\'s put some happy little bushes on the other side now. We\'ll paint one happy little tree right here. In life you need colors. Automatically, all of these beautiful, beautiful things will happen. This present moment is perfect simply due to the fact you\'re experiencing it. Isn\'t that fantastic? You can just push a little tree out of your brush like that. Just let go - and fall like a little waterfall. We want to use a lot pressure while using no pressure at all. This is your world, whatever makes you happy you can put in it. Go crazy. Let that brush dance around there and play. Pretend you\'re water. Just floating without any effort. Having a good day. We don\'t have to be concerned about it. We just have to let it fall where it will. I like to beat the brush. Maybe there\'s a little something happening right here. See there, told you that would be easy. Let\'s put some happy little clouds in our world. There is no right or wrong - as long as it makes you happy and doesn\'t hurt anyone. Look around, look at what we have. Beauty is everywhere, you only have to look to see it. You don\'t want to kill all your dark areas they are very important. That is when you can experience true joy, when you have no fear. You can get away with a lot. Only eight colors that you need. We\'ll have a super time. Work that paint. All you have to do is let your imagination go wild.'
@@ -32,15 +35,39 @@ class App extends Component {
             faq3: 'Suus satis. Quod etiam optime. Vos ite post eum, fistulae, nunquam vivum exire ab ea. Sed cum hoc ... excidit tibi in cibo aut in potu, aut: olefac Elegantioris non sit ... triginta sex horae post ... Poof. Vir aetatis operantes, dura sicut facit ... nemo mirabatur. Mike suspectas habere possunt, sed quod omnes illi eris. Obsecro, unum homicidam maniaco tempore.'
         })), delay.faq3);
     }
+
+    componentDidMount() {
+        ajax({url: 'https://api.github.com/users/kovcic?client_id=8a6b65d1f52e440fba8e&client_secret=50c87ef75640625225e26de657502e602be60e48', type: 'json'}, (resp) => {
+            if (resp.status === 200) {
+                this.setState({
+                    data: resp.data,
+                });
+            } else {
+                this.setState({
+                    err: resp.error + ' ' + JSON.stringify(resp.data) + ' ' + JSON.stringify(resp.headers),
+                });
+            }
+        });
+
+    }
+
     render() {
         const customerQuote = document.getElementById('root').getAttribute('data-customer-quote');
-        const { choMessage, faq1, faq2, faq3 } = this.state;
+        const { choMessage, faq1, faq2, faq3, data, err } = this.state;
         return (
             <div className="App">
                 <div className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
                     <h1>Welcome to Argelpargel!</h1>
                 </div>
+                {data &&
+                    <p>
+                        Loaded data of: {data.name}
+                    </p>}
+                {err &&
+                    <p>
+                        Error loading api: {err.toString()}
+                    </p>}
                 <p className="App-intro">
                     You've come to the right place to hang out with your friends,
                     chill and talk about Argel and Pargel. This is clearly a Good Thing<sup>TM</sup>
